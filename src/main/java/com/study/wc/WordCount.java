bbefb0089829d529d2a6d94c94634b7e0db7511b
shisq <shhz1994@163.com>
2021-03-01 17:09:37 +0800

    dataSet方式

---
+++ src/main/java/com/study/wc/WordCount.java
@@ -0,0 +1,30 @@
+package com.study.wc;
+
+import com.study.util.StringCountFlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+
+/**
+ * @author shsq
+ * @description: WordCount 单词计数
+ * @date 2021/3/1 15:52
+ */
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+        // 执行环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        // 文件中读取数据
+        String filePath = "D:\\data\\gitStudyData\\study_java_flink\\src\\main\\resources\\words";
+        DataSet<String> stringDataSet = env.readTextFile(filePath);
+
+        // 计数操作 以空格分隔 转换为 (word,1) 计算统计数
+        DataSet<Tuple2<String, Integer>> sumDataSet = stringDataSet.flatMap(new StringCountFlatMapFunction())
+                .groupBy(0)
+                .sum(1);
+
+        // 结果打印到控制台
+        sumDataSet.print();
+    }
+}
